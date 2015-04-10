f28cf956f4eb30cfc338203800cdde7799e1d29f
Daniel Lozano <mastermiw@192.168.7.31>
2015-04-10 17:44:17 +0200

    Extract Method con Replace los metodos de amyor y menor With Heritage (Clases vacias)

---
+++ src/intervals/ClosedPoint.java
@@ -0,0 +1,10 @@
+package intervals;
+
+public class ClosedPoint extends Point{
+
+	public ClosedPoint(double value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/intervals/OpenedPoint.java
@@ -0,0 +1,10 @@
+package intervals;
+
+public class OpenedPoint extends Point{
+
+	public OpenedPoint(double value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
